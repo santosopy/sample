dfdeb94b06e066d37864fbeba48afec001f5f652
santosopy <santoso@tsart.jp>
2022-06-30 14:58:18 +0900

    edit route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +17,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/', function () {
+    $posts = Post::all();
+
+    return view('welcome', compact("posts"));
+});
+
+Route::post('/', function (Request $request) {
+    $post = Post::firstOrNew(["name" => $request->name]);
+    $post->name = $request->name;
+    $post->save();
+    
+    $list = [];
+    if( isset($request->data) ){
+        foreach ($request->data as $key => $value) {
+            $tag = Tag::firstOrNew(["name" => $key]);
+            $tag->name = $key;
+            $list[] = $tag;
+        }
+    }
+    $tag2 = Tag::firstOrNew(["name" => $request->tagnameRaw]);
+    $tag2->name = $request->tagnameRaw;
+    $list[] = $tag2;
+    $post->tags()->saveMany($list);
+
+    return redirect()->back();
+});
